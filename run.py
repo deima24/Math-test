1fb40947aabc02a77cc872c36ef9b44667760b83
deima24 <12deima24@gmail.com>
2022-12-11 00:42:47 +0000

    creating begining and creating funtions for user selection

--- run.py
+++ run.py
@@ -3,8 +3,17 @@ print("Welcome to the Math test!")
 # Asking user what they want to do read the rules or start the game
 print("1) Start the game")
 print("2) Game rules")
-game_beginning = int(input("Please select if you want to start the game or read the rules of the game. "))
+game_beginning = input("Please select if you want to start the game or read the rules of the game. ")
 
 # Checking what user has typed if it typed 1 or 2
-if game_beginning != 1 and game_beginning != 2:
-    print("Invalid choose. Please try again")
+def game_start():
+        print("What level of Math quiz do you want to play? ")
+        print("1) Level 1 (0 - 100)")
+        print("2) Level 2 (100 - 1000)")
+
+while game_beginning not in ("1", "2"):
+    print("Select one of the two options:")
+if game_beginning == "1":
+    game_start()
+elif game_beginning == "2":
+    game_rules()
